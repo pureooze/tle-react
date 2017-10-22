7b0ac2dfdcca878c161f03d73f7d444aa8913f79
Uzair Shamim <uzashamim@gmail.com>
2017-10-22 15:49:44 -0400

    Added storybook and enzyme deps

---
+++ src/stories/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { specs, describe, it } from 'storybook-addon-specifications'
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import {mount} from "enzyme";
+import expect from "expect";
+
+const stories = storiesOf('Button', module);
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
